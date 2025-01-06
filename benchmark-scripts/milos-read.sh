473ffedea1b5c5543e140562416537f434e394e9
Birte Kristina Friesel <birte.friesel@uos.de>
2025-01-06 08:14:26 +0100

    benchmark-scripts: milos: include AVX512 measurements

--- benchmark-scripts/milos-read.sh
+++ benchmark-scripts/milos-read.sh
@@ -3,10 +3,19 @@
 mkdir -p log/${HOST}
 fn=log/${HOST}/read
 
-make -B numa=1 pthread=1
+make -B numa=1 pthread=1 avx512=1
 
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t4 4096 \
+	::: ram_in $(seq 0 15) \
+	:::+ ram_out $(seq 0 15) \
+	::: cpu $(seq 0 7) \
+	::: nr_threads $(seq 1 16) \
+>> ${fn}.txt
+
+fn=log/${HOST}/read-avx512
+parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
+	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t6 4096 \
 	::: ram_in $(seq 0 15) \
 	:::+ ram_out $(seq 0 15) \
 	::: cpu $(seq 0 7) \
--- benchmark-scripts/milos-roofline.sh
+++ benchmark-scripts/milos-roofline.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
 mkdir -p log
-fn=log/${HOST}-roofline
+fn=log/${HOST}-memcpy
 
-make -B numa=1 pthread=1
+make -B numa=1 pthread=1 avx512=1
 
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t0 4096 \
+	::: ram_in $(seq 0 15) \
+	::: ram_out $(seq 0 15) \
+	::: cpu $(seq 0 7) \
+	::: nr_threads $(seq 1 16) \
+>> ${fn}.txt
+
+fn=log/${HOST}-copy-avx512
+
+parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
+	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t3 4096 \
 	::: ram_in $(seq 0 15) \
 	::: ram_out $(seq 0 15) \
 	::: cpu $(seq 0 7) \
