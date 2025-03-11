718284f3697e03418ca25df557efc07df3d15829
Birte Kristina Friesel <birte.friesel@uos.de>
2025-03-11 14:01:36 +0100

    benchmark scripts: milos: Add CXL.mem card (in legacy mode) on NUMA node 16

--- benchmark-scripts/milos-write.sh
+++ benchmark-scripts/milos-write.sh
@@ -7,8 +7,8 @@ make -B numa=1 pthread=1 avx512=1
 
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t5 4096 \
-	::: ram_out $(seq 0 15) \
-	:::+ ram_in $(seq 0 15) \
+	::: ram_out $(seq 0 16) \
+	:::+ ram_in $(seq 0 16) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads $(seq 1 16) \
 >> ${fn}.txt
@@ -17,8 +17,8 @@ fn=log/${HOST}/write-avx512
 
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t7 4096 \
-	::: ram_out $(seq 0 15) \
-	:::+ ram_in $(seq 0 15) \
+	::: ram_out $(seq 0 16) \
+	:::+ ram_in $(seq 0 16) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads $(seq 1 16) \
 >> ${fn}.txt
