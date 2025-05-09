f9d26be0e2b01d7e83f2cab9c2ab21b1fb020a0c
Birte Kristina Friesel <birte.friesel@uos.de>
2025-05-09 07:54:15 +0200

    benchmark scripts: Add second CXL card; log git revision

--- benchmark-scripts/milos-copy.sh
+++ benchmark-scripts/milos-copy.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-mkdir -p log
-fn=log/${HOST}-memcpy
+mkdir -p log/${HOST}
 
 make -B numa=1 pthread=1 avx512=1
 
+fn=log/${HOST}/copy-memcpy
+echo "\n${fn}\n"
+echo "mbw $(git describe --all --long) $(git rev-parse HEAD)" >> ${fn}.txt
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
-	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t0 4096 \
-	::: ram_in $(seq 0 16) \
-	::: ram_out $(seq 0 16) \
+	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 5 -N {nr_threads} -t0 4096 \
+	::: ram_in $(seq 0 17) \
+	::: ram_out $(seq 0 17) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads $(seq 1 16) \
 >> ${fn}.txt
 
-fn=log/${HOST}-copy-avx512
-
+fn=log/${HOST}/copy-avx512
+echo "\n${fn}\n"
+echo "mbw $(git describe --all --long) $(git rev-parse HEAD)" >> ${fn}.txt
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
-	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t3 4096 \
-	::: ram_in $(seq 0 16) \
-	::: ram_out $(seq 0 16) \
+	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 5 -N {nr_threads} -t3 4096 \
+	::: ram_in $(seq 0 17) \
+	::: ram_out $(seq 0 17) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads $(seq 1 16) \
 >> ${fn}.txt
--- benchmark-scripts/milos-write.sh
+++ benchmark-scripts/milos-write.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
 mkdir -p log/${HOST}
-fn=log/${HOST}/write
 
 make -B numa=1 pthread=1 avx512=1
 
+fn=log/${HOST}/write-64bit
+echo "\n${fn}\n"
+echo "mbw $(git describe --all --long) $(git rev-parse HEAD)" >> ${fn}.txt
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t5 4096 \
-	::: ram_out $(seq 0 16) \
-	:::+ ram_in $(seq 0 16) \
+	::: ram_out $(seq 0 17) \
+	:::+ ram_in $(seq 0 17) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads $(seq 1 16) \
 >> ${fn}.txt
 
 fn=log/${HOST}/write-avx512
-
+echo "\n${fn}\n"
+echo "mbw $(git describe --all --long) $(git rev-parse HEAD)" >> ${fn}.txt
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
 	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t7 4096 \
-	::: ram_out $(seq 0 16) \
-	:::+ ram_in $(seq 0 16) \
+	::: ram_out $(seq 0 17) \
+	:::+ ram_in $(seq 0 17) \
 	::: cpu $(seq 0 7) \
 	::: nr_threads $(seq 1 16) \
 >> ${fn}.txt
