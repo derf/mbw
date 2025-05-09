47bf7ce8bafeb33acc23dd06ba9e002577042114
Birte Kristina Friesel <birte.friesel@uos.de>
2025-05-09 07:57:15 +0200

    milos-copy: reduce nr_threads configuration space

--- benchmark-scripts/milos-copy.sh
+++ benchmark-scripts/milos-copy.sh
@@ -8,20 +8,20 @@ fn=log/${HOST}/copy-memcpy
 echo "\n${fn}\n"
 echo "mbw $(git describe --all --long) $(git rev-parse HEAD)" >> ${fn}.txt
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
-	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 5 -N {nr_threads} -t0 4096 \
+	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t0 4096 \
 	::: ram_in $(seq 0 17) \
 	::: ram_out $(seq 0 17) \
 	::: cpu $(seq 0 7) \
-	::: nr_threads $(seq 1 16) \
+	::: nr_threads 1 2 4 6 8 10 12 14 16 \
 >> ${fn}.txt
 
 fn=log/${HOST}/copy-avx512
 echo "\n${fn}\n"
 echo "mbw $(git describe --all --long) $(git rev-parse HEAD)" >> ${fn}.txt
 parallel -j1 --eta --joblog ${fn}.joblog --resume --header : \
-	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 5 -N {nr_threads} -t3 4096 \
+	./mbw -a {ram_in} -b {ram_out} -c {cpu} -n 10 -N {nr_threads} -t3 4096 \
 	::: ram_in $(seq 0 17) \
 	::: ram_out $(seq 0 17) \
 	::: cpu $(seq 0 7) \
-	::: nr_threads $(seq 1 16) \
+	::: nr_threads 1 2 4 6 8 10 12 14 16 \
 >> ${fn}.txt
